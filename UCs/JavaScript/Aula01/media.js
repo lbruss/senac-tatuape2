17ef276ed5cb67fb42fa490bfb0fef92d2cf918d
Bruss Loza Carrasco <brusslcarrasco@gmail.com>
2026-08-25 16:46:39 -0300

    Update

---
+++ UCs/JavaScript/Aula01/media.js
@@ -0,0 +1,3 @@
+let num1 = 5, num2 = 6, num3 = 2, num4 = 3;
+var media = (num1+num2+num3+num4) / 4;
+console.log("A média é:",media);
---
+++ UCs/JavaScript/Aula01/olamundo.js
@@ -0,0 +1,36 @@
+console.log("Olá mundo!");
+var a=10, b=5; //Criando variáveis
+console.log(a); //Comando usado para exibir o valor armazenado na variável 'a'
+console.log(b); //Comando usado para exibir o valor armazenado na variável 'b'
+console.log("O resultado de a+b é:",a+b); //Comando usado para exibir o resultado do cálculo da adição 
+console.log(a-b); //Comando usado para exibir o resultado do cálculo da subtração
+console.log(a*b); //Comando usado para exibir o resultado do cálculo da multiplicação
+console.log(a/b); //Comando usado para exibir o resultado do cálculo da divisão exibindo o quociente
+console.log(a%b); //Comando usado para exibir o resultado do cálculo da divisão exibindo o resto
+console.log(a**b); //Comando usado para exibir o resultado do cálculo de um valor elevado a potência
+
+// *** Regras de Sintaxe - JavaScript*** //
+//Outra forma de declarar variáveis
+//Podemos usar o comando var ou let
+let x = 5;
+let y = 6;
+
+let z = x + y;
+console.log(z);
+
+//Os número pordem ser exibidos da seguinte forma:
+//10 - número inteiro
+//10.50 - número decimal
+
+//A String são texto, e podem ser escritas entre aspas duplas ou simples
+// 'Bruss'
+// "Bruss"
+
+//Variáveis
+let numero
+numero = 10;
+
+//JavaScript é sensivel a maiúsculas e minúsculas
+//Exemplo:
+let nome = "Bruss";
+let Nome = "Loza";
