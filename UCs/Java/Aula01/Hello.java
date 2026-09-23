74c63d8916bf7b5ca19db2d476778bcec51fbd80
Bruss Loza Carrasco <brusslcarrasco@gmail.com>
2026-09-23 16:06:18 -0300

    Update

---
+++ UCs/Java/Aula01/Hello.java
@@ -0,0 +1,9 @@
+/** Para fazer cometário
+Hello World!
+@autor Bruss Loza*/
+
+public class Hello{
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+    }
+}
